e85dc1477dd0542051dc92803745b544a703d529
kYaRick <kilyshuk@gmail.com>
2021-10-18 00:25:20 +0300

    Test form and chart were added.

---
+++ DIAnalyze_lab_2/DIA_lab_2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIAnalyze_lab_2
+{
+    enum Method
+    {
+        DefFunc
+    }
+    class DIA_lab_2
+    {
+        private float[] intr = new float[2];
+
+        public DIA_lab_2() {}
+
+        private double Func (double x)
+        {
+            return Math.Cos(0.1 * x) + Math.Cos(x);
+        }
+
+        public List<double[]> GetArrPoints(float sPointX, float fPointX, float step, Method flag)
+        {
+            intr[0] = sPointX;
+            intr[1] = fPointX;
+
+            List<double[]> Points = new List <double[]>();
+            while (intr[0] < intr[1])
+            {
+                switch (flag)
+                {
+                    case Method.DefFunc:
+                        {
+                            Points.Add(new double[] { 
+                                    Math.Round(intr[0], 2), 
+                                    Math.Round(Func(intr[0]), 2)
+                            });
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                intr[0] += step;
+            }
+
+            return Points;
+        }
+    }
+}
---
+++ DIAnalyze_lab_2/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DIAnalyze_lab_2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var a = new DIA_lab_2();
+            var MyChart = a.GetArrPoints(0f, 10f, 0.1f, Method.DefFunc);
+
+            chart1.Series[0].Points.Clear();
+
+            foreach (var item in MyChart)
+            {
+                chart1.Series[0].Points.AddXY(item[0], item[1]);
+            }
+        }
+    }
+}
